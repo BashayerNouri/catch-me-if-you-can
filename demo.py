1ec587d3648313b01a785d233c8c4587699e3f7f
Bashayer Nouri <bashayer_nouri@hotmail.com>
2020-04-06 09:53:33 +0400

    added solution

---
+++ demo.py
@@ -0,0 +1,18 @@
+
+print('Hello, World!') # prints a message 
+
+print('Hi, My name is Bashayer') # prints a greeting message
+
+print(True) # prints a boolean
+
+print(4) # prints an integer
+
+print(9.11) # prints a float point number
+
+print(['red','green','blue']) # prints a list
+
+print(('Apple','Bannan','Orange')) # prints a tuple
+
+print({1, 2, 3}) # prints a set 
+
+print({'brand': 'volkswagen', 'model': 'jetta', 'year': 2013}) # prints a dictionary 
